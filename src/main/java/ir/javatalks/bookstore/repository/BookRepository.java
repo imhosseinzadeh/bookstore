dc5fbd4e722ed88c984c0fe69e7af74278c07cc7
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-17 19:35:10 +0330

    feat: use pagination features to load books

--- src/main/java/ir/javatalks/bookstore/repository/BookRepository.java
+++ src/main/java/ir/javatalks/bookstore/repository/BookRepository.java
@@ -2,8 +2,9 @@ package ir.javatalks.bookstore.repository;
 
 import ir.javatalks.bookstore.entity.Book;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long>, PagingAndSortingRepository<Book, Long> {
 }
--- src/main/java/ir/javatalks/bookstore/service/BookService.java
+++ src/main/java/ir/javatalks/bookstore/service/BookService.java
@@ -1,4 +1,22 @@
 package ir.javatalks.bookstore.service;
 
+import ir.javatalks.bookstore.entity.Book;
+import ir.javatalks.bookstore.repository.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Page<Book> findAllBookPages(Pageable pageable) {
+        return bookRepository.findAll(pageable);
+    }
+
 }
