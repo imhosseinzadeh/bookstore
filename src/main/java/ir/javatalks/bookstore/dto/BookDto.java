d38321b15374185c0c3602bd47e842d8319956c1
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-18 17:32:41 +0330

    feat: add book dto and book mapper

--- src/main/java/ir/javatalks/bookstore/dto/BookDto.java
+++ src/main/java/ir/javatalks/bookstore/dto/BookDto.java
@@ -1,4 +1,18 @@
 package ir.javatalks.bookstore.dto;
 
+import ir.javatalks.bookstore.entity.BookSubject;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDto {
+
+    private String title;
+    private String author;
+    private BookSubject subject;
 }
--- src/main/java/ir/javatalks/bookstore/mapper/BookMapper.java
+++ src/main/java/ir/javatalks/bookstore/mapper/BookMapper.java
@@ -1,8 +1,12 @@
 package ir.javatalks.bookstore.mapper;
 
+import ir.javatalks.bookstore.dto.BookDto;
+import ir.javatalks.bookstore.entity.Book;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
+    BookDto mapToDto(Book book);
 
+    Book mapToModel(BookDto bookDto);
 }
