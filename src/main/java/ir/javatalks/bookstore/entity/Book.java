070aa0a290af490025cf8c5dfaacdfffc97e8ccf
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-19 12:12:48 +0330

    refactor: use string type of enum

--- src/main/java/ir/javatalks/bookstore/entity/Book.java
+++ src/main/java/ir/javatalks/bookstore/entity/Book.java
@@ -1,6 +1,8 @@
 package ir.javatalks.bookstore.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
@@ -16,6 +18,7 @@ public class Book {
     private Long id;
     private String title;
     private String author;
+    @Enumerated(EnumType.STRING)
     private BookSubject subject;
     @PositiveOrZero
     private int quantity;
