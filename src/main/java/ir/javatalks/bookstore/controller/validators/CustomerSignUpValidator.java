1105f2459d863e4b84b230775de0de318b291133
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-23 13:01:40 +0330

    fix: use annotated param to match super method

--- src/main/java/ir/javatalks/bookstore/controller/validators/CustomerSignUpValidator.java
+++ src/main/java/ir/javatalks/bookstore/controller/validators/CustomerSignUpValidator.java
@@ -2,6 +2,7 @@ package ir.javatalks.bookstore.controller.validators;
 
 import ir.javatalks.bookstore.dto.CustomerDto;
 import ir.javatalks.bookstore.service.CustomerService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -14,12 +15,12 @@ public class CustomerSignUpValidator implements Validator {
     private final CustomerService customerService;
 
     @Override
-    public boolean supports(Class<?> clazz) {
+    public boolean supports(@NotNull Class<?> clazz) {
         return CustomerDto.class.isAssignableFrom(clazz);
     }
 
     @Override
-    public void validate(Object target, Errors errors) {
+    public void validate(@NotNull Object target, @NotNull Errors errors) {
         CustomerDto customerDto = (CustomerDto) target;
         if (customerService.findByEmail(customerDto.getEmail()).isPresent()) {
             errors.reject("email", "Email already exists");
