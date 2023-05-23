68dd99a57639f8fdb8406a2d052dfed990192847
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-23 13:00:20 +0330

    feat: set request mapping

--- src/main/java/ir/javatalks/bookstore/controller/BookController.java
+++ src/main/java/ir/javatalks/bookstore/controller/BookController.java
@@ -5,19 +5,16 @@ import ir.javatalks.bookstore.mapper.BookMapper;
 import ir.javatalks.bookstore.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+@RestController("/books")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
     private final BookMapper bookMapper;
 
-    public BookController(BookService bookService, BookMapper bookMapper) {
-        this.bookService = bookService;
-        this.bookMapper = bookMapper;
-    }
-
+    @GetMapping("/find-all")
     public Page<Book> findAllBooks(@RequestParam(defaultValue = "0") Integer pageNo,
                                    @RequestParam(defaultValue = "10") Integer pageSize) {
         return bookService.findAllBooks(pageNo, pageSize);
