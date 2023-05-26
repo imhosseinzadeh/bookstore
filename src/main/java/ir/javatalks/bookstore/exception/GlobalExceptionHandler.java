bc89a0add59623d53921cc21fed4a27ae11a8f3c
Iman Hosseinzadeh <imhosseinzadeh@gmail.com>
2023-05-26 19:43:30 +0330

    feat: add exception handler for authentication problems

--- src/main/java/ir/javatalks/bookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/ir/javatalks/bookstore/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package ir.javatalks.bookstore.exception;
 
 import ir.javatalks.bookstore.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,14 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ErrorResponseDto handleMyException(Exception ex) {
-        return new ErrorResponseDto("400", ex.getMessage());
+        return new ErrorResponseDto(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), ex.getMessage());
+    }
+
+    @ExceptionHandler(value = {AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseBody
+    public ErrorResponseDto handleMyException(AccessDeniedException ex) {
+        return new ErrorResponseDto("403", ex.getMessage());
     }
 
 }
